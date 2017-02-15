d99077770e6374801739e9c6aa15c8972243eb8a
Yicun Qian <kamoonwalka@gmail.com>
2017-02-15 15:03:38 -0500

    updated hadoop 2.7

--- tachyon/init.sh
+++ tachyon/init.sh
@@ -53,7 +53,7 @@ else
       elif [[ "$HADOOP_MAJOR_VERSION" == "2" ]]; then
         wget https://s3.amazonaws.com/Tachyon/tachyon-$TACHYON_VERSION-cdh4-bin.tar.gz
       else
-        wget https://s3.amazonaws.com/Tachyon/tachyon-$TACHYON_VERSION-hadoop2.4-bin.tar.gz
+        wget https://s3.amazonaws.com/Tachyon/tachyon-$TACHYON_VERSION-hadoop2.7-bin.tar.gz
       fi
       if [ $? != 0 ]; then
         echo "ERROR: Unknown Tachyon version"
